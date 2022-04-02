40878d24aae3e413dc4dd1f94c039c4bc8883015
Annie Huang <annie.hn@gmail.com>
2022-04-02 15:02:11 +1100

    Use React 18 syntax in index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const container = document.getElementById('app');
+
+// Create a root.
+const root = createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App tab='home' />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
