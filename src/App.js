e3b691fd27df4665de28ac696cf102a5d8588f1d
Annie Huang <annie.hn@gmail.com>
2022-04-02 17:34:03 +1100

    useRef -> memory leak problem of on when the component make a api call, but the component is unmounted before the api comes back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import UseRefExample1 from './components/UseRefExample1';
 import UseRefExample2 from './components/UseRefExample2';
+import UseRefExample3 from './components/UseRefExample3';
 
 function App() {
   return (
     <div className='container mt-5'>
       <UseRefExample1 />
+      <hr style={{ margin: '40px 0', color: 'red' }} />
       <UseRefExample2 />
+      <hr style={{ margin: '40px 0', color: 'red' }} />
+      <UseRefExample3 />
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+const Todo = () => {
+  const [loading, setLoading] = useState(true);
+  const [todo, setTodo] = useState({});
+
+  // If I click 'Toggle Todo' Button within 3 seconds, I will get this error:
+  // index.js:1 Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+  //     at Todo (http://localhost:3000/static/js/main.chunk.js:688:87)
+  //
+  // However, react 18 seems to resolve this problem!?!
+  // You can see this problem in react 17: C:\Temp\my-react17-app\src\components\Todo.tsx
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          setTodo(data);
+          setLoading(false);
+        }, 3000);
+      });
+  }, []);
+
+  return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>;
+};
+
+export default Todo;
