82dd75b1e82d155eda8e216375842f946cec8666
Annie Huang <annie.hn@gmail.com>
2022-04-02 17:51:04 +1100

    useRef -> Solution for memory leak in UserRefExample3.jsx and Todo.jsx. React 18 seems to resolve this problem.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-const Todo = () => {
+/*const Todo = () => {
   const [loading, setLoading] = useState(true);
   const [todo, setTodo] = useState({});
 
@@ -21,6 +21,40 @@ const Todo = () => {
         }, 3000);
       });
   }, []);
+
+  return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>;
+};*/
+
+const Todo = () => {
+  const [loading, setLoading] = useState(true);
+  const [todo, setTodo] = useState({});
+
+  // If I click 'Toggle Todo' Button within 3 seconds, I will get this error:
+  // index.js:1 Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+  //     at Todo (http://localhost:3000/static/js/main.chunk.js:688:87)
+  //
+  // However, react 18 seems to resolve this problem!?!
+  // You can see this problem in react 17: C:\Temp\my-react17-app\src\components\Todo.tsx
+
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          if (isMounted.current) {
+            setTodo(data);
+            setLoading(false);
+          }
+        }, 3000);
+      });
+
+    // This runs when component is unmounted
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>;
 };
